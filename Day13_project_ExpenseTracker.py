32c4d80fea6f36da1b730b50b445f6b47cc204db
Noor Ismot Ara <ara.noor.ismot@gmail.com>
2025-12-25 21:02:43 +0100

    Update Day13_project_ExpenseTracker.py

--- Day13_project_ExpenseTracker.py
+++ Day13_project_ExpenseTracker.py
@@ -29,32 +29,19 @@ Constraints:
 
 =======================================================================
 """
-expense_amount = []
-expense_category = []
-expense_description = []
-expense_per_category ={
-     
-}
+expenses =[
+]
+expense_per_category ={}
 
 def add_expense(item_number):
-    total_expense_amount = 0
+    
     for i in range(item_number):
         x, y, z = input(f"For Item {i+1}, write your expense amount in dollar, category and description: ").split(", ")
-        expense_amount.append(x)
-        expense_category.append(y)
-        expense_description.append(z)
-        expense_per_category[y] = x
-        total_expense_amount += int(x)
-          
-
+        expenses.append({"Amount": x, "Category": y, "Des": z})
     print(f"\nExpense Added Successfully!\n")
-    print(expense_per_category)
-    return total_expense_amount
-    
-
-def view_all_expense(item_number):   
-    for i in range(int(item_number)):
-        print(f"Catgory: {expense_category[i]}, Amount: ${expense_amount[i]}, Description: {expense_description[i]}")
+    print(expenses)
+ 
+            
 
 
 def user_item():
@@ -64,11 +51,7 @@ def user_item():
                     if int(expense_item) <= 0:
                         print("Item number need to be larger than Zero")
                     else:
-                        total_cost = add_expense(int(expense_item))
-                        print(f"Your total expense: {total_cost}")
-                        print("Your Expense List:")
-                        view_all_expense(expense_item)
-                        
+                        all_expense = add_expense(int(expense_item))                       
         else:
             print("Enter a valid number.")
 
@@ -82,4 +65,3 @@ def main():
         
 
 main()
-
