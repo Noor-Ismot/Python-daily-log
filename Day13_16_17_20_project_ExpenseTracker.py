48cb80c759b868bcfdd64e5245fa2823d50d0739
Noor Ismot Ara <ara.noor.ismot@gmail.com>
2026-01-04 19:54:32 +0100

    Update Day13_16_17_20_project_ExpenseTracker.py

--- Day13_16_17_20_project_ExpenseTracker.py
+++ Day13_16_17_20_project_ExpenseTracker.py
@@ -1,6 +1,6 @@
 """
 Project Name: Personal Expense Tracker
-========================================================================
+=========================================================
 Objectives:
 
 Practice lists & dictionaries
@@ -15,23 +15,22 @@ Understand data aggregation
 Requirements:-
 
 User can:
--Add an expense (amount, category, description) : DONE
--View all expenses : DONE
--View total expenses : DONE
+-Add an expense (amount, category, description)
+-View total expenses
 -View total per category
 
 Constraints:
--Store expenses in memory only (no files)
+-Store expenses in in files
 -Amount must be a positive number 
 -Category must be a non-empty string
 
-=======================================================================
+=========================================================
 """
 def view_category_expense(expense_list):
     expense_per_category ={}
     for expense in expense_list:
-            category = expense["Category"]
-            amount = expense["Amount"]
+            category = expense["category"]
+            amount = expense["amount"]
             if category in expense_per_category:
                 expense_per_category.update({category:expense_per_category[category] + amount})
              
@@ -40,25 +39,26 @@ def view_category_expense(expense_list):
     return expense_per_category
 
 
-def view_total_expense(expense_list):  
-    total_expense = 0
-    for expense in expense_list:
-        amount = expense["Amount"]
-        total_expense += amount   
-    return total_expense
+def view_total(total_expense):
+    total = 0
+    for key in total_expense:
+        total += total_expense[key]
+    
+    return total
+
 
 
 def add_expense(item_number):   
     expenses =[]   
     for i in range(item_number):
-        x, y, z = input(f"For Item {i+1}, write your expense amount in dollar, category and description: ").split(", ")
-        if x.isdigit() and len(y)>0 :
-            x = int(x)
-            y = y.lower()
-            expenses.append({"Amount": x, "Category": y, "Des": z})
+        amount, category, description = input(f"For Item {i+1}, write your expense amount in dollar, category and description: ").split(", ")
+        if amount.isdigit() and len(category) > 0 :
+            amount = int(amount)
+            category = category.lower()
+            expenses.append({"amount": amount, "category": category, "description": description})
         else:
             print("Enter a positive integer number with Non empty category.")
-            x, y, z = input(f"For Item {i+1}, write your expense amount in dollar, category and description: ").split(", ")
+            amount, category, description = input(f"For Item {i+1}, write your expense amount in dollar, category and description: ").split(", ")
     
     print(f"\nExpense Added Successfully!\n")    
     return expenses
@@ -79,14 +79,6 @@ def user_item():
     
     all_expense = add_expense(int(expense_item))
     return all_expense
-
-
-def view_total(total_expense):
-    total = 0
-    for key in total_expense:
-        total += total_expense[key]
-    
-    return total
 
 
 
